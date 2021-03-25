0406ed25f74bed1db853fdafbcefbc5bc5b115ef
learn-crystallography <learn-crystallography@users.noreply.github.com>
2021-03-25 12:44:59 +0000

    jekyll build from Action 360bcbe7e9e792d82ac2a40eed23aa424ba30c09

---
+++ _pages/index.md
@@ -0,0 +1,1 @@
+## Welcome to the BCA Education Resources site
---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,79 @@
+var store = [{
+        "title": "Post: Modified Date",
+        "excerpt":"This post has been updated and should show a modified date if used in a layout. All children, except one, grow up. They soon know that they will grow up, and the way Wendy knew was this. One day when she was two years old she was playing in a...","categories": ["Blog"],
+        "tags": ["Post Formats","readability","standard"],
+        "url": "/pages/learn-crystallography/blog/post-modified/",
+        "teaser": null
+      },{
+        "title": "Post: Standard",
+        "excerpt":"All children, except one, grow up. They soon know that they will grow up, and the way Wendy knew was this. One day when she was two years old she was playing in a garden, and she plucked another flower and ran with it to her mother. I suppose she...","categories": ["Blog"],
+        "tags": ["Post Formats","readability","standard"],
+        "url": "/pages/learn-crystallography/blog/post-standard/",
+        "teaser": null
+      },{
+        "title": "Post: Chat",
+        "excerpt":"Abbott: Strange as it may seem, they give ball players nowadays very peculiar names. Costello: Funny names? Abbott: Nicknames, nicknames. Now, on the St. Louis team we have Who’s on first, What’s on second, I Don’t Know is on third– Costello: That’s what I want to find out. I want...","categories": ["Blog"],
+        "tags": ["chat","Post Formats"],
+        "url": "/pages/learn-crystallography/blog/post-chat/",
+        "teaser": null
+      },{
+        "title": "Post: Notice",
+        "excerpt":"A notice displays information that explains nearby content. Often used to call attention to a particular detail. When using Kramdown {: .notice} can be added after a sentence to assign the .notice to the &lt;p&gt;&lt;/p&gt; element. Changes in Service: We just updated our privacy policy here to better service our...","categories": ["Blog"],
+        "tags": ["Post Formats","notice"],
+        "url": "/pages/learn-crystallography/blog/post-notice/",
+        "teaser": null
+      },{
+        "title": "Post: Quote",
+        "excerpt":"   Only one thing is impossible for God: To find any sense in any copyright law on the planet.       Mark Twain   ","categories": ["Blog"],
+        "tags": ["Post Formats","quote"],
+        "url": "/pages/learn-crystallography/blog/post-quote/",
+        "teaser": null
+      },{
+        "title": "Post: Link",
+        "excerpt":"This theme supports link posts, made famous by John Gruber. To use, just add link: http://url-you-want-linked to the post’s YAML front matter and you’re done.      And this is how a quote looks.    Some link can also be shown.  ","categories": ["Blog"],
+        "tags": ["link","Post Formats"],
+        "url": "/pages/learn-crystallography/blog/post-link/",
+        "teaser": null
+      },{
+        "title": "Great British Innovations: Crystallography",
+        "excerpt":"An online poll identified X-ray crystallography as one of the top five most important innovations in science and technology from the last 100 years.  ","categories": [],
+        "tags": [],
+        "url": "/pages/learn-crystallography/great-british-innovations-crystallography/",
+        "teaser": null
+      },{
+        "title": "Diamond Light Source celebrates 100 years of Crystallography",
+        "excerpt":"That scientific principle of diffraction of X-rays by crytals, discovered in 1913, by father and son William and Lawrence Bragg has made possible much of the materials research that today takes place at Diamond Light Source. To mark the occasion, Dr John Jenkin, the biographer of the British scientific duo,...","categories": [],
+        "tags": [],
+        "url": "/pages/learn-crystallography/diamond-light-source-celebrates-100-years-of-crystallography/",
+        "teaser": null
+      },{
+        "title": "Crystallography365",
+        "excerpt":"Crystallography365 are blogging a crystal structure a day to celebrate the International Year of Crystallography.  Check out their website!  ","categories": [],
+        "tags": ["Crystal","Structure"],
+        "url": "/pages/learn-crystallography/crystallography365/",
+        "teaser": null
+      },{
+        "title": "Woman's Hour Collection: Dame Kathleen Lonsdale",
+        "excerpt":"Dame Kathleen Lonsdale was a pioneering woman scientist in the field of crystallography. Her discovery in 1929 that the benzene ring was flat, using X-ray diffraction methods, led to her being considered as one of the leading women in science. She was one of the first women elected as a...","categories": [],
+        "tags": [],
+        "url": "/pages/learn-crystallography/womans-hour-collection-dame-kathleen-lonsdale/",
+        "teaser": null
+      },{
+        "title": "The Royal Institution Crystallography Collection",
+        "excerpt":"The Royal Institution, in collaboration with STFC, have a number of crystallography related videos on their YouTube channel. More recently they have created two videos entitled 'Understanding Crystallography: Part 1 and Part 2'.  These are great videos, the first of which features Professor Elspeth Garman from the University of Oxford,...","categories": [],
+        "tags": [],
+        "url": "/pages/learn-crystallography/the-royal-institution-crystallography-collection/",
+        "teaser": null
+      },{
+        "title": "The Braggs' Legacy",
+        "excerpt":"Learn all about crystallography from a movie made by Diamond Light Source to celebrate 100 years of crystallography!               ","categories": [],
+        "tags": [],
+        "url": "/pages/learn-crystallography/the-braggs-legacy/",
+        "teaser": null
+      },{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["blog"],
+        "tags": ["Jekyll","update"],
+        "url": "/pages/learn-crystallography/blog/welcome-to-jekyll/",
+        "teaser": null
+      }]
